a2e4f0312c054ad9f8bfe58396ed60d4a84edadd
Lastijk <sanyalastijk@mail.ru>
2025-09-15 00:02:55 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SmallLibrarySystem.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmallLibrarySystem.dir/Book.cpp.o"
+  "CMakeFiles/SmallLibrarySystem.dir/Book.cpp.o.d"
+  "CMakeFiles/SmallLibrarySystem.dir/Library.cpp.o"
+  "CMakeFiles/SmallLibrarySystem.dir/Library.cpp.o.d"
+  "CMakeFiles/SmallLibrarySystem.dir/Rules.cpp.o"
+  "CMakeFiles/SmallLibrarySystem.dir/Rules.cpp.o.d"
+  "CMakeFiles/SmallLibrarySystem.dir/Users.cpp.o"
+  "CMakeFiles/SmallLibrarySystem.dir/Users.cpp.o.d"
+  "CMakeFiles/SmallLibrarySystem.dir/main.cpp.o"
+  "CMakeFiles/SmallLibrarySystem.dir/main.cpp.o.d"
+  "SmallLibrarySystem"
+  "SmallLibrarySystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmallLibrarySystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SmallLibrarySystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SmallLibrarySystem.
+# This may be replaced when dependencies are built.
